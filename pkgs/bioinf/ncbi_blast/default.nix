24266e46856ca43c27e1c0f738a61b69148b760c
Pavel Chuprikov <pschuprikov@gmail.com>
2019-12-16 19:27:48 +0100

    fix blast url

--- pkgs/bioinf/ncbi_blast/default.nix
+++ pkgs/bioinf/ncbi_blast/default.nix
@@ -1,9 +1,9 @@
 { stdenv, fetchurl, coreutils, procps, cpio }:
 stdenv.mkDerivation rec {
-  version = "2.9.0+";
+  version = "2.9.0";
   name = "ncbi-blast-${version}";
   src = fetchurl {
-    url = "https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/${name}-src.tar.gz";
+    url = "https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/${version}/${name}+-src.tar.gz";
     sha256 = "sha256:0my2rpd1bln05sxnp4c5wk5j5y6yx56vi38pzicjfhh9g8nwr453";
   };
 
