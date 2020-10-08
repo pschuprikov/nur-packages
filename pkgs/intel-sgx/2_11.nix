f29862209ad5d38358329e27ea2484de6e57a964
Pavel Chuprikov <pschuprikov@gmail.com>
2020-10-08 11:57:35 +0200

    fix hash

--- pkgs/intel-sgx/2_11.nix
+++ pkgs/intel-sgx/2_11.nix
@@ -1,7 +1,7 @@
 import ./generic.nix {
   version = "2.11";
   hasMitigation = true;
-  sha256 = "009hlkgnn3wvbsnawpfcwdxyncax9mb260vmh9anb91lmqbj74rp";
+  sha256 = "0nrz9wcvyccchwygxa6ng9726l9wlpf7f3nsblff2xwhsq6q1312";
   optlibsSha256 = "0qki299mxdp937vwg3163c79qj9976hdd6qs72j7h7jc25chiba3";
   binutilsSha256 = "1q1mf7p8xvwfkvw0fkvbv28q3wffwaz9c96s7hqv7r3095cj7xlp";
   patchOpenMP = true;
