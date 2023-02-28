91977a0cc367841e0750b2a6b5232696b1c7462a
Pavel Chuprikov <pschuprikov@gmail.com>
2023-02-28 19:07:46 +0000

    add multi-arch

--- flake.nix
+++ flake.nix
@@ -3,24 +3,26 @@
   inputs.nixpkgs.url = "github:pschuprikov/nixpkgs/nixos-22.11";
   inputs.mvn2nix.url = "github:fzakaria/mvn2nix";
   inputs.mvn2nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, mvn2nix }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-        overlays = [ mvn2nix.overlay ];
-      };
-      lib = pkgs.lib;
-      nur = import self { nixpkgsPath = nixpkgs; pkgs = pkgs; };
-      intelSGXPackages_2_7_1 =
-        nur.intelSGXPackages_2_7_1.override { debugMode = true; };
-    in {
-      packages.${system} =
-        lib.filterAttrs (n: d: lib.isDerivation d && !(d.meta.broken or false))
-        (nur // nur.qt5);
-      inherit (nur) overlays;
-      nixosModules = import ./modules;
-    };
+  outputs = { self, nixpkgs, mvn2nix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ mvn2nix.overlay ];
+        };
+        lib = pkgs.lib;
+        nur = import self { nixpkgsPath = nixpkgs; pkgs = pkgs; };
+        intelSGXPackages_2_7_1 =
+          nur.intelSGXPackages_2_7_1.override { debugMode = true; };
+      in {
+        packages =
+          lib.filterAttrs (n: d: lib.isDerivation d && !(d.meta.broken or false))
+          (nur // nur.qt5);
+        inherit (nur) overlays;
+        nixosModules = import ./modules;
+      }
+  );
 }
