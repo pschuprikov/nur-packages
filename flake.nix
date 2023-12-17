fcc73d10329723c2edbe98ec552da369d3507f50
Pavel Chuprikov <pschuprikov@gmail.com>
2023-12-17 10:33:57 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "NUR flake";
-  inputs.nixpkgs.url = "github:pschuprikov/nixpkgs/nixos-22.11";
+  inputs.nixpkgs.url = "github:pschuprikov/nixpkgs/nixos-23.05";
   inputs.mvn2nix.url = "github:fzakaria/mvn2nix";
   inputs.mvn2nix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
@@ -11,12 +11,14 @@
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
+          config.permittedInsecurePackages = [
+                "openssl-1.1.1u" 
+                "python-2.7.18.6"
+          ];
           overlays = [ mvn2nix.overlay ];
         };
         lib = pkgs.lib;
         nur = import self { nixpkgsPath = nixpkgs; pkgs = pkgs; };
-        intelSGXPackages_2_7_1 =
-          nur.intelSGXPackages_2_7_1.override { debugMode = true; };
       in {
         packages =
           lib.filterAttrs (n: d: lib.isDerivation d && !(d.meta.broken or false))
